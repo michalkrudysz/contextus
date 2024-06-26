09ce503c6681eaceb34c219f7cb850747c548f9b
Michal <michal@drontrzebownisko.pl>
2024-06-26 21:32:28 +0200

    Added logs to verify the connection to the database.

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,11 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config();
 import mysql from "mysql2/promise";
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+
+let pool;
+try {
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+  pool
+    .getConnection()
+    .then((connection) => {
+      console.log("DB connection successful.");
+      connection.release();
+    })
+    .catch((err) => {
+      console.error("Error when connecting to the DB:", err);
+    });
+} catch (err) {
+  console.error("Error while creating the connection pool:", err);
+}
 
 export default pool;
