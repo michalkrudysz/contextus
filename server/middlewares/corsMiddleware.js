a2978446385871a4b29a57dfa373c4f7f2bf31c0
Michal <michal@drontrzebownisko.pl>
2024-06-27 16:28:22 +0200

    CORS support was added in Nginx, so I commented out the CORS settings on the backend to avoid conflicts.

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -1,16 +1,16 @@
-import cors from "cors";
+// import cors from "cors";
 
-const corsOptions = {
-  origin: "https://contextus.pl",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  optionsSuccessStatus: 200,
-  preflightContinue: false,
-  onHeader: function (req, res) {
-    if (!res.headersSent) {
-      console.log("Setting CORS headers for request:", req.path);
-    }
-  },
-};
+// const corsOptions = {
+//   origin: "https://contextus.pl",
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   optionsSuccessStatus: 200,
+//   preflightContinue: false,
+//   onHeader: function (req, res) {
+//     if (!res.headersSent) {
+//       console.log("Setting CORS headers for request:", req.path);
+//     }
+//   },
+// };
 
-export default cors(corsOptions);
+// export default cors(corsOptions);
