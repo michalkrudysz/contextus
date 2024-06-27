70d9dc7e749a04fc6c1d85b3ee066d1b103d4f5a
Michal <michal@drontrzebownisko.pl>
2024-06-27 15:57:39 +0200

    Continued updating of the corsMiddleware.

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -1,13 +1,16 @@
-// Importowanie modułu cors
 import cors from "cors";
 
-// Definicja opcji CORS, które pozwolą na przyjmowanie żądań z konkretnej domeny
 const corsOptions = {
-  origin: "https://contextus.pl", // Tylko żądania z tej domeny będą akceptowane
-  methods: ["GET", "POST", "PUT", "DELETE"], // Dozwolone metody
-  allowedHeaders: ["Content-Type", "Authorization"], // Dozwolone nagłówki
-  optionsSuccessStatus: 200, // Dla przeglądarek legacy wymagających tego statusu dla żądań OPTIONS
+  origin: "https://contextus.pl",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
+  preflightContinue: false,
+  onHeader: function (req, res) {
+    if (!res.headersSent) {
+      console.log("Setting CORS headers for request:", req.path);
+    }
+  },
 };
 
-// Eksportowanie skonfigurowanego middleware CORS
 export default cors(corsOptions);
